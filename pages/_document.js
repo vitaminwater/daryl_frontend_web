23f63933f49223c346ae449d9be3795bc5df3257
vitaminwater <stantin04@gmail.com>
2017-12-18 09:20:33 +0100

    fixed fton

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,23 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>My page</title>
+          <title>Daryl</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <link rel="icon" 
+            type="image/png" 
+            href="/static/favicon.png" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
+          <style>{`
+            textarea, input, button { outline: none; }
+            textarea:focus, input:focus, button:focus { outline: none; }
+            body {margin: 0; padding: 0; overflow-x: hidden; font-family: Roboto Light, Helvetica Neue, Helvetica, Arial, sans-serif, -apple-system;}
+
+            ::-webkit-input-placeholder { color:#D1D1D1; }
+            ::-moz-placeholder { color:#D1D1D1; } /* firefox 19+ */
+            :-ms-input-placeholder { color:#D1D1D1; } /* ie */
+            input:-moz-placeholder { color:#D1D1D1; }
+          `}</style>
           {this.props.styleTags}
         </Head>
         <body>
